93410e4c609c8e4db3d492f319ad563abd52d8a7
jacasho22 <jacasho22@hotmail.com>
2025-08-15 00:49:02 +0200

    Change BrowserRouter to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './pages/pages.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
